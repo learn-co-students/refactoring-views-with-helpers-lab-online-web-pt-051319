603fa6feb295f86cd7c8f5b36c9ed52fd755f880
Jason McGloin <jason.mcgloin@gmail.com>
2019-10-15 21:09:27 -0400

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,8 @@
 module SongsHelper
 
+	def display_artist(song)
+		# binding.pry
+		song.artist ? link_to(song.artist_name, artist_path(song.artist)) : link_to('Add Artist', edit_song_path(song))
+	end
+
 end
