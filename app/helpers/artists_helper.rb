cec7f0b8dd18c8b25eb218ec7aa1960a57bb27e1
ablakerose <bdecktor@gmail.com>
2019-10-22 11:48:41 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,19 @@
 module ArtistsHelper
+    
+    def display_artist(song)
+        if song.artist
+            link_to song.artist.name, artist_path(song.artist)
+        else
+            link_to "Add Artist", edit_song_path(song)
+        end
+    end
+ 
 end
+
+# def display_artist(artist)
+#     if @artist.name
+#         link_to @artist.name, artist_path(@artist)
+#     else
+#         link_to "Add Artist", edit_song_path(@artist.song)
+#     end
+# end
